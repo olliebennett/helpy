2520767d64db35cb7f029dc7a45ea6c66be54e80
rusikf <rs41@gmx.com>
2016-04-08 19:23:40 +0300

    move methods to stats helper

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,8 @@ class AdminController < ApplicationController
 
   layout 'admin'
 
+  include StatsHelper
+  
   before_action :authenticate_user!
   before_action :verify_admin
   before_action :fetch_counts, :only => ['dashboard','tickets','ticket', 'update_ticket', 'topic_search', 'user_profile']
@@ -24,6 +26,7 @@ class AdminController < ApplicationController
       .group('topics.id')
       .having('COUNT(posts.id) > 1')
       .ids
+
     @responded_topics = Topic.where(id: responded_topic_ids)
     @closed_topic_count = @topics.closed.count
 
@@ -457,13 +460,6 @@ class AdminController < ApplicationController
     else
       @topics = Topic.where(current_status: @status).page params[:page]
     end
-  end
-
-  # See: http://stackoverflow.com/a/14859546/1323144
-  def median(array)
-    sorted = array.sort
-    len = sorted.length
-    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
   end
 
 end
